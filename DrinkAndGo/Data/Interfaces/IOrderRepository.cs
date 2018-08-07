9a6b93a34d9eb440f8612b02c95ee2e50ebb2a75
girishgodage <girishgodage@gmail.com>
2018-08-07 10:12:05 +0530

    Add project files.

---
+++ DrinkAndGo/Data/Interfaces/IOrderRepository.cs
@@ -0,0 +1,14 @@
+﻿using DrinkAndGo.Data.Models;
+using DrinkAndGo.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DrinkAndGo.Data.Interfaces
+{
+    public interface IOrderRepository
+    {
+        void CreateOrder(Order order);
+    }
+}
---
+++ DrinkAndGo/Startup.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using DrinkAndGo.Data.Interfaces;
+using DrinkAndGo.Data.Mocks;
+using Microsoft.Extensions.FileProviders;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using DrinkAndGo.Data;
+using Microsoft.EntityFrameworkCore;
+using DrinkAndGo.Data.Repositories;
+using DrinkAndGo.Data.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace DrinkAndGo
+{
+    public class Startup
+    {
+
+        private IConfigurationRoot _configurationRoot;
+
+        public Startup(IHostingEnvironment hostingEnvironment)
+        {
+            _configurationRoot = new ConfigurationBuilder()
+                .SetBasePath(hostingEnvironment.ContentRootPath)
+                .AddJsonFile("appsettings.json")
+                .Build();
+        }
+
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //Server configuration
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(_configurationRoot.GetConnectionString("DefaultConnection")));
+            //Authentication, Identity config
+            services.AddIdentity<IdentityUser, IdentityRole>()
+                .AddEntityFrameworkStores<AppDbContext>();
+
+            services.AddTransient<ICategoryRepository, CategoryRepository>();
+            services.AddTransient<IDrinkRepository, DrinkRepository>();
+
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddScoped(sp => ShoppingCart.GetCart(sp));
+            services.AddTransient<IOrderRepository, OrderRepository>();
+
+            services.AddMvc();
+            services.AddMemoryCache();
+            services.AddSession();
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IServiceProvider serviceProvider)
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseStatusCodePages();
+            app.UseStaticFiles();
+            app.UseSession();
+            app.UseIdentity();
+
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                   name: "drinkdetails",
+                   template: "Drink/Details/{drinkId?}",
+                   defaults: new { Controller = "Drink", action = "Details" });
+
+                routes.MapRoute(
+                    name: "categoryfilter",
+                    template: "Drink/{action}/{category?}",
+                    defaults: new { Controller = "Drink", action = "List" });
+
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{Id?}");
+            });
+
+            DbInitializer.Seed(serviceProvider);
+        }
+    }
+}
